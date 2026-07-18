27143c31b3e6e49e6013e572eebf667e9aef0a00
Emjay Nerizon <emjaynerizon61@gmail.com>
2026-07-18 18:06:29 +0800

    debugging

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/debug-app-key', function () {
+    return [
+        'app_key_env' => env('APP_KEY'),
+        'app_key_config' => config('app.key'),
+    ];
+});
